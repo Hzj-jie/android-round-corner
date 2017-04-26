fc605e027e6e3a6373e130a8f8232969b3b1be4e
Zijie He <zijiehe@chromium.org>
2017-04-25 20:07:49 -0700

    Implementation

--- src/main/java/org/gemini/round_corner/RCService.java
+++ src/main/java/org/gemini/round_corner/RCService.java
@@ -1,14 +1,18 @@
 package org.gemini.round_corner;
 
+import android.annotation.TargetApi;
 import android.app.Service;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.PixelFormat;
+import android.graphics.Point;
 import android.os.IBinder;
 import android.view.Gravity;
 import android.view.WindowManager;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class RCService extends Service {
+  private static final AtomicInteger running = new AtomicInteger();
   private RCView tl;
   private RCView tr;
   private RCView br;
@@ -17,14 +21,6 @@ public class RCService extends Service {
   @Override
   public IBinder onBind(final Intent intent) {
     return null;
-  }
-
-  private static int screenWidth() {
-    return Resources.getSystem().getDisplayMetrics().widthPixels;
-  }
-
-  private static int screenHeight() {
-    return Resources.getSystem().getDisplayMetrics().heightPixels;
   }
 
   private static int gravity(final float degree) {
@@ -41,7 +37,7 @@ public class RCService extends Service {
     */
   }
 
-  private static int x(final float degree, final int size) {
+  private int x(final float degree, final int size) {
     switch ((int)degree) {
       case 0: return 0;
       case 90: return screenWidth() - size;
@@ -52,7 +48,7 @@ public class RCService extends Service {
     return 0;
   }
 
-  private static int y(final float degree, final int size) {
+  private int y(final float degree, final int size) {
     switch ((int)degree) {
       case 0: return 0;
       case 90: return 0;
@@ -63,15 +59,46 @@ public class RCService extends Service {
     return 0;
   }
 
+  @SuppressWarnings( "deprecation" )
+  @TargetApi(17)
+  private int screenWidth() {
+    WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+    try {
+      Point size = new Point();
+      wm.getDefaultDisplay().getRealSize(size);
+      return size.x;
+    } catch (Exception ex) {
+      return wm.getDefaultDisplay().getWidth();
+    }
+  }
+
+  @SuppressWarnings( "deprecation" )
+  @TargetApi(17)
+  private int screenHeight() {
+    WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+    try {
+      Point size = new Point();
+      wm.getDefaultDisplay().getRealSize(size);
+      return size.y;
+    } catch (Exception ex) {
+      return wm.getDefaultDisplay().getHeight();
+    }
+  }
+
   private RCView bind(final WindowManager wm, final float degree) {
     RCView v = new RCView(this, degree);
     WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-        v.size(),
-        v.size(),
-        x(degree, v.size()),
-        y(degree, v.size()),
+        v.width(),
+        v.height(),
+        x(degree, v.width()),
+        y(degree, v.height()),
         WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
-        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE |
+        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+        WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
+        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS |
+        WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
         PixelFormat.TRANSPARENT);
     params.gravity = gravity(degree);
     params.setTitle("RCView-" + degree);
@@ -96,5 +123,16 @@ public class RCService extends Service {
     ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(tr);
     ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(br);
     ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(bl);
+  }
+
+  @Override
+  public int onStartCommand(
+      final Intent intent, final int flags, final int startId) {
+    if (running.compareAndSet(0, 1)) {
+      return START_STICKY;
+    } else {
+      stopSelf(startId);
+      return START_NOT_STICKY;
+    }
   }
 }
--- src/main/java/org/gemini/round_corner/RCView.java
+++ src/main/java/org/gemini/round_corner/RCView.java
@@ -1,28 +1,82 @@
 package org.gemini.round_corner;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 
 public class RCView extends View {
+  private static final String TAG = "RoundCornerView";
+  private static final boolean DEBUGGING = false;
   private final Paint paint;
+  private final float degrees;
+  private final Bitmap bitmap;
+  private final Matrix matrix;
+  private final int width;
+  private final int height;
 
-  public RCView(final Context context, final float degree) {
+  public RCView(final Context context, final float degrees) {
     super(context);
-    paint = new Paint();
-    paint.setAntiAlias(true);
-    paint.setTextSize(10);
-    paint.setARGB(255, 255, 0, 0);
+    if (DEBUGGING) {
+      this.paint = new Paint();
+      this.paint.setAntiAlias(true);
+      this.paint.setTextSize(10);
+      this.paint.setARGB(255, 255, 0, 0);
+    } else {
+      this.paint = null;
+    }
+    this.degrees = degrees;
+    this.bitmap = loadImage();
+    this.matrix = new Matrix();
+    this.matrix.postRotate(degrees, bitmap.getWidth() / 2, bitmap.getHeight() / 2);
+    this.matrix.postScale(scale(), scale());
+    // Log.w(TAG, "bitmap size " + bitmap.getWidth() + " x " + bitmap.getHeight());
+    this.width = (int)(bitmap.getWidth() * scale());
+    this.height = (int)(bitmap.getHeight() * scale());
   }
 
-  public int size() {
-    return 100;
+  private Bitmap loadImage() {
+    Bitmap bitmap = loadImage(degrees + ".png");
+    if (bitmap != null) {
+      return bitmap;
+    }
+    bitmap = loadImage(".png");
+    if (bitmap != null) {
+      return bitmap;
+    }
+    return BitmapFactory.decodeResource(getResources(), R.drawable.default_png);
+  }
+
+  private static Bitmap loadImage(final String type) {
+    return BitmapFactory.decodeFile(
+        Environment.DIRECTORY_PICTURES + "/org.gemini.round_corner" + type);
+  }
+
+  public int width() {
+    return width;
+  }
+
+  public int height() {
+    return height;
+  }
+
+  private float scale() {
+    // return Resources.getSystem().getDisplayMetrics().density;
+    return 1;
   }
 
   @Override
   protected void onDraw(final Canvas canvas) {
     super.onDraw(canvas);
-    canvas.drawText("Hello World", 5, 15, paint);
+    canvas.drawBitmap(bitmap, matrix, null);
+    if (DEBUGGING) {
+      canvas.drawText("org.gemini.round_corner", 5, 15, paint);
+    }
   }
 }
