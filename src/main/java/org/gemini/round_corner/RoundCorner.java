f043e630c4afc46834dfc55f7b23c7dee81463fa
Zijie He <zijiehe@chromium.org>
2017-05-01 12:25:32 -0700

    Formatting

--- src/main/java/org/gemini/round_corner/RoundCorner.java
+++ src/main/java/org/gemini/round_corner/RoundCorner.java
@@ -4,11 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class RoundCorner extends Activity
-{
+public class RoundCorner extends Activity {
     @Override
-    protected void onCreate(Bundle bundle)
-    {
+    protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         startService(new Intent(this, RCService.class));
         finish();
